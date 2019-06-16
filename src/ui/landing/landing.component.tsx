574f37e224cd8fd6344fad41a2af3159118f8fec
Anton Koshkin <akoshkin@devexperts.com>
2019-06-16 15:50:35 +0300

    become a speaker base

--- src/ui/landing/landing.component.tsx
+++ src/ui/landing/landing.component.tsx
@@ -3,11 +3,11 @@ import * as React from 'react';
 import {Header} from '../header/header/header.component';
 import {Promo} from '../promo/promo.component';
 import {Schedule} from '../schedule/schedule/schedule.component';
-import {Speakers} from '../speakers/speakers.comonent';
 import {Sponsors} from '../sponsors/sponsors.component';
 import {Footer} from '../footer/footer.component';
 import {data} from '../../view-models/data.view-model';
 import {About} from '../about/about.component';
+import {BecomeASpeaker} from '../become-a-speaker/become-a-speaker.component';
 
 export const Landing: FC = memo(() =>
 	data
@@ -17,7 +17,7 @@ export const Landing: FC = memo(() =>
 				<Promo data={data} />
 				<About data={data} />
 				<Schedule data={data} />
-				<Speakers />
+				<BecomeASpeaker />
 				<Sponsors />
 				<Footer />
 			</Fragment>
--- src/ui/speakers/speakers.comonent.tsx
+++
@@ -1,4 +0,0 @@
-import {FC, memo} from 'react';
-import * as React from 'react';
-
-export const Speakers: FC<{className?: string}> = memo(({className}) => <div>Speakers</div>);
