af24d70756597cf290b32ec8e3ad81c9711d3ffa
Anton Koshkin <akoshkin@devexperts.com>
2019-07-02 13:36:26 +0300

    facelift

--- src/ui/ui-kit/block-heading/block-heading.component.tsx
+++ src/ui/ui-kit/block-heading/block-heading.component.tsx
@@ -2,22 +2,35 @@ import {FC, memo} from 'react';
 import styled from '@emotion/styled';
 import * as React from 'react';
 import {mediaMd} from '../../../utils/css.utils';
+import {Lines} from '../lines/lines.component';
 
 //#region styled
 const BlockHeadingStyled = styled.h2`
 	text-transform: uppercase;
 	color: var(--purpur);
 	font-size: 36px;
-	margin-left: 38px;
 	font-weight: 600;
 
 	${mediaMd} {
 		font-size: 80px;
-		margin-left: 90px;
+	}
+`;
+const LinesStyled = styled(Lines)`
+	transform: scaleY(-1);
+	display: inline-block;
+	width: 38px;
+	margin-right: 2px;
+
+	${mediaMd} {
+		width: 76px;
+		margin-right: 10px;
 	}
 `;
 //#endregion
 
 export const BlockHeading: FC<{className?: string}> = memo(({children, className}) => (
-	<BlockHeadingStyled className={className}>{children}</BlockHeadingStyled>
+	<BlockHeadingStyled className={className}>
+		<LinesStyled />
+		{children}
+	</BlockHeadingStyled>
 ));
