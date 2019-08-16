8958f9c9731855baece4ba7bfe1917674cebd956
Vadim Gorbachev <bmsdave@gmail.com>
2019-08-16 23:01:06 +0300

    fix offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,8 +72,6 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-offline`,
 			options: {
-				navigateFallback: null,
-				navigateFallbackWhitelist: [],
 				cacheId: 'gatsby-plugin-offline-0',
 			},
 		},
