e155fb33687580e40480bb8fdf636e8398254039
Vadim Gorbachev <bmsdave@gmail.com>
2019-08-16 19:43:31 +0300

    add google analytics part2

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,29 +78,9 @@ module.exports = {
 		// `gatsby-plugin-offline`,
 		`gatsby-plugin-remove-serviceworker`,
 		{
-			resolve: `gatsby-plugin-google-analytics`,
+			resolve: 'gatsby-plugin-google-analytics',
 			options: {
-				trackingId: "UA-97990001-2",
-				// Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: false,
-				// Setting this parameter is optional
-				// anonymize: true,
-				// Setting this parameter is also optional
-				// respectDNT: true,
-				// Avoids sending pageview hits from custom paths
-				exclude: [],
-				// Delays sending pageview hits on route update (in milliseconds)
-				pageTransitionDelay: 0,
-				// Enables Google Optimize using your container Id
-				// optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-				// Enables Google Optimize Experiment ID
-				// experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-				// Set Variation ID. 0 for original 1,2,3....
-				// variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-				// Any additional optional fields
-				sampleRate: 5,
-				siteSpeedSampleRate: 10,
-				cookieDomain: "conf.piterjs.org",
+				trackingId: 'UA-97990001-2',
 			},
 		},
 	],
